08afab9fc81a62458141c2199bc71a7efcf9549d
zeke <swb830712@gmail.com>
2023-03-08 13:51:07 +0800

    annotation Finish

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from "react";
-import Data from "./Response.json";
-import TitleData from "./ResponseLiftHeader.json";
+import Data from "./Response.json"; // 原始資料
+import TitleData from "./ResponseLiftHeader.json"; // 原始標題資料
 import moment from "moment/moment";
 
 const Homepage = () => {
+  // 顯示或隱藏加減乘除
   const [showDiscountForm, setShowDiscountForm] = useState(false);
+  // 定位各個表格內容的位置
   const [highLightIndex, setHighLightIndex] = useState([null, null, null]);
+  // 把原始資料綁監聽
   const [jsonData, setData] = useState(Data);
+  // select 選擇價格
   const [showDataView, setShowDataView] = useState("rates");
+  // roomType 的選擇
   const [roomType, setRoomType] = useState("");
+  // 為了對應每隔input跟同列的資料綁定 做的迴圈
+  // 把每格input 輸入的時候都更改陣列的內容 預設為1(乘法)
+  // 做成二維陣列
   const [multiplyPrice, setMultiplyPrice] = useState(function () {
     const row = [];
     jsonData.list.data.map((res) => {
@@ -32,14 +40,18 @@ const Homepage = () => {
     return row;
   });
 
+  // 給屬於六日的index 放值的陣列
   let holiday = [];
 
+  // 更動值不動原陣列原則 把原本陣列 引入temp內
+  // i j k 做定位可以抓輸入的值在json的位置 然後改變
+  // 最後在導回去Data
   function changPrice(e, iIndex, jIndex, kIndex) {
     let temp = Object.assign({}, jsonData);
     temp.list.data[iIndex][jIndex][kIndex][0].rates = e.target.value;
     setData(temp);
   }
-
+  // 取 乘法預設的陣列 導進該列位置
   function setMultiply(e, i, j) {
     let temp = [...multiplyPrice];
     temp[i][j] = e;
@@ -52,6 +64,9 @@ const Homepage = () => {
     setAddPrice(temp);
   }
 
+  // input 裡面的值 3個if 為了做價格改變時功能依舊
+  // 分別為 rates extraadult extrachild
+  // 先把第一格的金額取出 用map跑陣列數量的迴圈把值帶進去
   function updataPrice(i, j) {
     if (showDataView === "rates") {
       let price = Number(jsonData.list.data[i][j][0][0].rates);
@@ -82,6 +97,7 @@ const Homepage = () => {
       setData(temp);
     }
   }
+  // 歸類六日
   Data.list.inventory[0].forEach((item, index) => {
     if (
       moment(item.date).format("d") === "6" ||
@@ -89,7 +105,11 @@ const Homepage = () => {
     ) {
       holiday.push(index);
     }
-  }); // 歸類六日
+  });
+
+  // 把input 尚未儲值的值 變回儲存之前的樣子
+  // 如果本機有之前儲存的資料就拿出來沒有的話就把本地資料導入
+  // countryItem 為儲存時候的key 值
 
   function cleanButton() {
     if (!localStorage.getItem("countryItem")) {
@@ -101,11 +121,15 @@ const Homepage = () => {
     }
   }
 
+  // 把資料存進本地端
+  // 本地資料端只能存字串 先轉字串
+  // 再放進key值為 countryItem 的地方
   function submitButton() {
     let countryString = JSON.stringify(jsonData);
     localStorage.setItem("countryItem", countryString);
   }
 
+  // 渲染時如果線上端沒資料就拿本地端塞進去
   useEffect(() => {
     const items = JSON.parse(localStorage.getItem("countryItem"));
     if (items) {
@@ -118,6 +142,7 @@ const Homepage = () => {
   return (
     <div className="wrap">
       <div>
+        {/* 選擇顯示不同的價格 */}
         <select
           className="select"
           defaultValue="--select--"
@@ -134,6 +159,7 @@ const Homepage = () => {
           <option value="extraadult">extraadult</option>
           <option value="extrachild">extrachild</option>
         </select>
+        {/* 選擇roomTypr */}
         <select
           className="select"
           defaultValue="--select roomtpye--"
@@ -149,9 +175,11 @@ const Homepage = () => {
           <option value="Superior Queen">Superior Queen</option>
           <option value="test">test</option>
         </select>
+        {/* 清除尚未儲存的部分 */}
         <button type="button" className="button" onClick={cleanButton}>
           清除目前所輸入的值
         </button>
+        {/* 把目前有輸入的值儲存到本地端 */}
         <button type="button" className="button" onClick={submitButton}>
           save
         </button>
@@ -159,6 +187,7 @@ const Homepage = () => {
       <table>
         <tbody>
           <tr className="heard">
+            {/* 取最左邊的日期當左上角的顯示 */}
             <td>{Data.list.inventory[0][0].date}</td>
             {Data.list.inventory[0].map((date, hIndex) => (
               <>
@@ -169,6 +198,7 @@ const Homepage = () => {
                   ${holiday.includes(hIndex) ? "holidayBaccColor" : ""} 
                   `}
                 >
+                  {/* 把時間格式轉換 */}
                   <p>{moment(date.date).format("ddd")}</p>
                   <p>{moment(date.date).format("D")}</p>
                   <p>{moment(date.date).format("MMM ")}</p>
@@ -176,9 +206,11 @@ const Homepage = () => {
               </>
             ))}
           </tr>
+          {/* 第一層map 拆解 */}
           {jsonData.list.data.map((res, iIndex) => (
             <>
               <tr className="bodyBorder border-collapse  ">
+                {/* className 變換roomType時 有跟選擇的一樣 就不會隱藏 */}
                 <th
                   className={`textJustify ${
                     TitleData.list[iIndex].roomtype === roomType ||
@@ -187,6 +219,7 @@ const Homepage = () => {
                       : "datadisplay"
                   }`}
                 >
+                  {/* 第一欄的值 */}
                   <p style={{ display: "inline" }}>
                     {TitleData.list[iIndex].roomtype}
                   </p>
@@ -201,6 +234,7 @@ const Homepage = () => {
                     ></img>
                   </button>
                 </th>
+                {/* 第二層 map */}
                 {Data.list.inventory[iIndex].map((uaData, uaIndex) => (
                   <th
                     className={`${
@@ -212,12 +246,16 @@ const Homepage = () => {
                   ${holiday.includes(uaIndex) ? "holidayBaccColor" : ""} 
                   ${uaData.aval === "0" ? "textRed" : "textBlue"}`}
                   >
+                    {/* holiday 抓是六日的話背景顏色要變 */}
+                    {/* uaData 有 0/NA 做變色 */}
                     {uaData.aval}
                   </th>
                 ))}
               </tr>
+              {/* 第三層 */}
               {res.map((resData, jIndex) => (
                 <tr>
+                  {/* 第二個判斷再寫抓迴圈位子做hover的效果 */}
                   <td
                     className={`${
                       TitleData.list[iIndex].roomtype === roomType ||
@@ -233,7 +271,9 @@ const Homepage = () => {
                     }`}
                     style={{ textAlign: "left" }}
                   >
+                    {/* 左邊標題不是黑體字的顯示區塊 */}
                     {TitleData.list[iIndex].ratedata[jIndex].display_name}
+                    {/* form 表單隱藏或顯示 各會把自己本身的位置帶上去上面陣列做存放 */}
                     {showDiscountForm && (
                       <form className="textRight">
                         x{" "}
@@ -263,6 +303,7 @@ const Homepage = () => {
                       </form>
                     )}
                   </td>
+                  {/* 第四層 拆解 */}
                   {resData.map((dateData, kIndex) => (
                     <td
                       className={`
@@ -276,11 +317,13 @@ const Homepage = () => {
                       onMouseEnter={() =>
                         setHighLightIndex([iIndex, jIndex, kIndex])
                       }
+                      // onMouseEnter 滑鼠事件 觸發 把該滑鼠碰到的迴圈位子傳上去
                     >
                       <input
                         onChange={(event) =>
                           changPrice(event, iIndex, jIndex, kIndex)
                         }
+                        // value 判斷價格選擇的顯示
                         value={`${
                           showDataView === "rates" ? dateData[0].rates : ""
                         }${
